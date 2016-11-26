baae41c55443cfc743e32443d70f70ab4f035fea
Kiril Tsarnakliyski <ktsarnakliyski@gmail.com>
2016-11-26 17:58:42 +0200

    Add XUnit, Moq and simple test

---
+++ WebApp.API.Tests/ControllerTests/AccountControllerTests.cs
@@ -0,0 +1,36 @@
+using Xunit;
+using Moq;
+using Microsoft.AspNetCore.Identity;
+using WebApp.API.Models;
+using WebApp.API.Controllers;
+using WebApp.API.Services;
+using Microsoft.Extensions.Logging;
+using WebApp.API.Models.AccountViewModels;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebApp.API.Tests.ControllerTests
+{
+    public class AccountControllerTests
+    {
+        private readonly UserManager<ApplicationUser> userManagerMock = new FakeUserManager();
+        private readonly Mock<IAccountService> accountServiceMock = new Mock<IAccountService>();
+        private readonly Mock<IEmailSender> emailSenderMock = new Mock<IEmailSender>();
+        private readonly Mock<ILoggerFactory> loggerFactoryMock = new Mock<ILoggerFactory>();
+
+        [Fact]
+        public async Task Register_Should_Return_Ok()
+        {
+            //Assert
+            AccountController controller = new AccountController(userManagerMock, accountServiceMock.Object, emailSenderMock.Object, loggerFactoryMock.Object);
+
+            //Act
+            var result = await controller.Register(new RegisterViewModel());
+            var okResult = result as OkResult;
+
+            //Assert
+            Assert.NotNull(okResult);
+            Assert.Equal(200, okResult.StatusCode);
+        }
+    }
+}
