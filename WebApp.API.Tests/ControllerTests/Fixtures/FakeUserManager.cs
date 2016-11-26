2bf47670b3c03f2fe85bae13fe0d311bf3f8a3b4
Kiril Tsarnakliyski <ktsarnakliyski@gmail.com>
2016-11-26 17:58:42 +0200

    Add XUnit, Moq and simple test

---
+++ WebApp.API.Tests/ControllerTests/Fixtures/FakeUserManager.cs
@@ -0,0 +1,29 @@
+using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using Moq;
+using WebApp.API.Models;
+
+public class FakeUserManager : UserManager<ApplicationUser>
+{
+    public FakeUserManager()
+        : base(new Mock<IUserStore<ApplicationUser>>().Object,
+              new Mock<IOptions<IdentityOptions>>().Object,
+              new Mock<IPasswordHasher<ApplicationUser>>().Object,
+              new IUserValidator<ApplicationUser>[0],
+              new IPasswordValidator<ApplicationUser>[0],
+              new Mock<ILookupNormalizer>().Object,
+              new Mock<IdentityErrorDescriber>().Object,
+              new Mock<IServiceProvider>().Object,
+              new Mock<ILogger<UserManager<ApplicationUser>>>().Object)
+    { }
+
+    public override Task<IdentityResult> CreateAsync(ApplicationUser user, string password)
+    {
+        return Task.FromResult(IdentityResult.Success);
+    }
+}
