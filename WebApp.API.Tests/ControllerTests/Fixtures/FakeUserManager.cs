1418813b5038950fb9f887e370d2b40f51e3664c
Kiril Tsarnakliyski <ktsarnakliyski@gmail.com>
2016-12-03 19:14:11 +0200

    Fix namespace

--- WebApp.API.Tests/ControllerTests/Fixtures/FakeUserManager.cs
+++ WebApp.API.Tests/ControllerTests/Fixtures/FakeUserManager.cs
@@ -1,12 +1,11 @@
 using System;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 using Moq;
-using WebApp.API.Models;
+using WebApp.API.Models.Db;
 
 public class FakeUserManager : UserManager<ApplicationUser>
 {
