643eb53482a1cf66562d0a959d67b3e9facfd37b
Kiril Tsarnakliyski <ktsarnakliyski@gmail.com>
2016-12-15 00:43:46 +0200

    Remove usings

--- WebApp.API/Data/Migrations/ApplicationDbContextModelSnapshot.cs
+++ WebApp.API/Data/Migrations/ApplicationDbContextModelSnapshot.cs
@@ -2,8 +2,6 @@
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
-using Microsoft.EntityFrameworkCore.Migrations;
-using WebApp.API.Data;
 
 namespace WebApp.API.Data.Migrations
 {
