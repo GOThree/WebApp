c910b70d583c5ce1103f4a0579a52b190c80cdc0
Kiril Tsarnakliyski <ktsarnakliyski@gmail.com>
2016-12-14 23:44:13 +0200

    Update the way we use OpenIddictDbContext

--- WebApp.API/Data/ApplicationDbContext.cs
+++ WebApp.API/Data/ApplicationDbContext.cs
@@ -1,19 +1,18 @@
 using Microsoft.EntityFrameworkCore;
-using OpenIddict;
 using WebApp.API.Models.Db;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
 
 namespace WebApp.API.Data
 {
-    public class ApplicationDbContext : OpenIddictDbContext<ApplicationUser>
+    public class ApplicationDbContext : IdentityDbContext<ApplicationUser>
     {
-        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
-            : base(options)
-        {
-        }
+        public ApplicationDbContext(DbContextOptions options)
+            : base(options) { }
 
         protected override void OnModelCreating(ModelBuilder builder)
         {
             base.OnModelCreating(builder);
+
             // Customize the ASP.NET Identity model and override the defaults if needed.
             // For example, you can rename the ASP.NET Identity table names and more.
             // Add your customizations after calling base.OnModelCreating(builder);
